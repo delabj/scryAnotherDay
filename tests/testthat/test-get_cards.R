1b6447914465b6c690abbc20558fed89a0bae57e
delabj <j.l.delabruere@gmail.com>
2020-12-17 09:31:53 -0600

    Add tests for get_cards_by_name

--- tests/testthat/test-get_cards.R
+++ tests/testthat/test-get_cards.R
@@ -11,9 +11,27 @@ testthat::test_that("get_cards_by_search fails if arguments are the the wrong ty
   testthat::expect_error(get_cards_by_search(format = TRUE)) # should be Char
   testthat::expect_error(get_cards_by_search(page = "1")) # should be numeric
 })
-testthat::test_that("get_cards_by_search fails if arguments aren't in lists", {
+testthat::test_that("get_cards_by_search fails if arguments aren't in list options", {
   testthat::expect_error(get_cards_by_search(unique = "card"))
   testthat::expect_error(get_cards_by_search(order = "names"))
   testthat::expect_error(get_cards_by_search(dir = "automatic"))
   testthat::expect_error(get_cards_by_search(format = "xlsx"))
+})
+
+testthat::test_that("get_cards_by_name fails without a query", {
+  testthat::expect_error(get_cards_by_name(q = NULL))
+})
+testthat::test_that("get_cards_by_search fails if arguments are the the wrong type", {
+  testthat::expect_error(get_cards_by_name(search_type = 1))
+  testthat::expect_error(get_cards_by_name(set = 1))
+  testthat::expect_error(get_cards_by_name(format = 1))
+  testthat::expect_error(get_cards_by_name(face = 1))
+  testthat::expect_error(get_cards_by_name(version = 1))
+  testthat::expect_error(get_cards_by_name(pretty = "False"))
+})
+testthat::test_that("get_cards_by_search fails if arguments aren't in list options", {
+  testthat::expect_error(get_cards_by_name(search_type = "exactish"))
+  testthat::expect_error(get_cards_by_name(face = "two"))
+  testthat::expect_error(get_cards_by_name(face = "extra-medium"))
+  testthat::expect_error(get_cards_by_name(format = "xlsx"))
 })
