cb8aed86a16f12e56035e91bce019c8b5ff62338
delabj <j.l.delabruere@gmail.com>
2021-01-04 10:08:04 -0600

    Add get_sets()
    `get_sets()` returns a dataframe with all sets listed in scryfall.

--- R/get_sets.R
+++ R/get_sets.R
@@ -1,6 +1,31 @@
 
+
+
+
 ## To Add
-# /sets
 # /sets/:code
 # /sets/tcgplayer/:id
 # /sets/:id
+
+#' Get a list of all sets
+#'
+#' @description Fetch a list of all sets, and their respective codes/IDs, release date, and set attributes.
+#'
+#' @return a data.frame containing all listed sets
+#' @export
+get_sets <- function(){
+
+  check_internet()
+
+  res <- httr::GET(
+    url = "https://api.scryfall.com/sets"
+  )
+
+  check_status(res)
+
+  listed <- jsonlite::fromJSON(httr::content(res, "text", encoding = "UTF-8"))
+
+  attempt::stop_if_not(("data" %in% names(listed)), msg="Error in response from API")
+
+  return(listed$data)
+}
